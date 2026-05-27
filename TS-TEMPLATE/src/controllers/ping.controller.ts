f0168036b381b415cfdc856f39143e312baff0e3
astroophilliiiiiiiii <kritibansal318@gmail.com>
2026-05-27 13:26:55 +0000

    add routers

--- TS-TEMPLATE/src/controllers/ping.controller.ts
+++ TS-TEMPLATE/src/controllers/ping.controller.ts
@@ -1,5 +1,5 @@
 import { Request , Response } from "express"
 
 export const pingHandler = ( req:Request , res:Response ) =>{
-    res.send("pong") ; 
+    res.send("pong-gitcodespace") ; 
 }
--- TS-TEMPLATE/src/server.ts
+++ TS-TEMPLATE/src/server.ts
@@ -1,14 +1,16 @@
 import express from "express" ;
 import { serverConfig } from "./config/index.js";
 import { pingHandler } from "./controllers/ping.controller.js";
+import pingRouter from "./Routers/ping.router.js";
 
 const app = express() ; 
 
-const PORT = serverConfig.PORT ; 
+const PORT = serverConfig.PORT ;
 
-app.get("/ping" , pingHandler ) ; 
+app.use( pingRouter ) ; 
 
 app.listen(PORT , ()=>{
     console.log("Server is listening on port:- " , PORT ) ; 
 }) 
+
 
