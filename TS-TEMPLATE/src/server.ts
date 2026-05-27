368ca0faac2966d88dc294f03690bdb44fd946de
astroophilliiiiiiiii <kritibansal318@gmail.com>
2026-05-27 13:00:10 +0000

    add controllers

--- TS-TEMPLATE/src/server.ts
+++ TS-TEMPLATE/src/server.ts
@@ -1,10 +1,14 @@
 import express from "express" ;
 import { serverConfig } from "./config/index.js";
+import { pingHandler } from "./controllers/ping.controller.js";
 
 const app = express() ; 
 
 const PORT = serverConfig.PORT ; 
 
+app.get("/ping" , pingHandler ) ; 
+
 app.listen(PORT , ()=>{
     console.log("Server is listening on port:- " , PORT ) ; 
 }) 
+
